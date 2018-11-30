d756f21fe9502284c81cf005624f50b4750bdcb3
generous-program-0200 <harrycopley@vultuslimited.com>
2018-11-30 22:43:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+katz_deli = []
+
+
+def take_a_number(array, name)
+  array << name
+  number = array.index(name)
+  puts "Hello #{name}, you are #{number +1} in line."
+end
+
+take_a_number(katz_deli, "Harry")
+
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+
+
+
+=begin
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    
+end
+
+
+
+def now_serving(katz_deli)
+  puts "Currently serving #{name}."
+else
+  puts "There is nobody waiting to be servered!"
+end
+
+line(katz_deli)
+
+=end
